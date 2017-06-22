de42d349b98c1e09ebf80ea33ad3d5da7dd6aff5
Anna Fuentes Jakobsen <annafuentesjakobsen@Annas-MBP.wlan.int.chas.se>
2017-06-22 15:28:23 +0200

    added webpack

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+require('./index.css');
 
-class HelloWorld extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <div>Hello World!</div>
     )
   }
 }
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+ReactDOM.render(
+  <App />, 
+  document.getElementById('app')
+);
