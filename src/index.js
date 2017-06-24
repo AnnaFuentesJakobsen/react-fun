9a0f95736c8746161ea9c17630ad7f0e88fa872d
Anna Fuentes Jakobsen <annafjakobsen@gmail.com>
2017-06-24 22:19:41 +0200

    added friends list

--- src/index.js
+++ src/index.js
@@ -2,14 +2,46 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 
-class App extends React.Component {
+class Users extends React.Component {
   render() {
+    var friends = this.props.list.filter(function (user) {
+      return user.friend === true
+    });
+    
+    var nonFriends = this.props.list.filter(function (user) {
+       return user.friend === !true                                    
+    });
+    
     return (
-      <div>Hello World!</div>
+      <div>
+        <h1>Friends</h1>
+        <ul>
+          {friends.map(function (user) {
+            return <li key={user.name}>{user.name}</li>
+          })}
+        </ul>
+        
+        <hr />
+        
+        <h1> Non Friends </h1>
+        <ul>
+          {nonFriends.map(function (user) {
+            return <li key={user.name}>{user.name}</li>
+          })}
+        </ul>        
+      </div>
     )
   }
 }
+
 ReactDOM.render(
-  <App />, 
+  <Users list={[
+    { name: 'Tyler', friend: true },
+    { name: 'Ryan', friend: true },
+    { name: 'Michael', friend: false },
+    { name: 'Mikenzi', friend: false },
+    { name: 'Jessica', friend: true },
+    { name: 'Dan', friend: false } ]} 
+  />,
   document.getElementById('app')
 );
